15a329ebc1cfc0d0d8421de4a5ae2ad880d2a70a
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-27 11:21:53 -0500

    Initial commit

---
+++ src/main/java/com/sofka/democrudtest/DemoCrudTestApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.democrudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofka/democrudtest/DemoCrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.democrudtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
