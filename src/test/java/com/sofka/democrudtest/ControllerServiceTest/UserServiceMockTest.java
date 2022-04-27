c8bbd6d30423b015c8e8da16f4f032eaf1302755
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-27 14:23:41 -0500

    Pruebas
    Se agregan las pruebas utilizando la libreria junit y mockito

--- src/test/java/com/sofka/democrudtest/ControllerServiceTest/UserServiceMockTest.java
+++ src/test/java/com/sofka/democrudtest/ControllerServiceTest/UserServiceMockTest.java
@@ -1,2 +1,42 @@
-package com.sofka.democrudtest.ControllerServiceTest;public class UserServiceMockTest {
+package com.sofka.democrudtest.ControllerServiceTest;
+
+import com.sofka.democrudtest.controllers.UserController;
+import com.sofka.democrudtest.models.UserModel;
+import com.sofka.democrudtest.repositories.UserRepository;
+import com.sofka.democrudtest.services.UserService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserServiceMockTest {
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testUserMock(){
+        when(userRepository.findAll()).thenReturn(new ArrayList<>());
+        assertThat(userService.getUsers()).isEmpty();
+        verify(userRepository).findAll();
+    }
 }
--- src/test/java/com/sofka/democrudtest/ServicesTest/UserServiceTest.java
+++ src/test/java/com/sofka/democrudtest/ServicesTest/UserServiceTest.java
@@ -1,2 +1,43 @@
-package com.sofka.democrudtest.ServicesTest;public class UserServiceTest {
+package com.sofka.democrudtest.ServicesTest;
+
+
+import com.sofka.democrudtest.models.UserModel;
+import com.sofka.democrudtest.repositories.UserRepository;
+import com.sofka.democrudtest.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static  org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class UserServiceTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void testSaveUser(){
+        UserModel userModel=new UserModel("aquaman","aqua@gmail.com",99);
+        UserModel userModelSaved = userRepository.save(userModel);
+        assertNotNull(userModelSaved);
+    }
+
+    @Test
+    public void testSearchUserById(){
+        Long idSearched=1L;
+        Optional<UserModel> userModelSearched=userRepository.findById(idSearched);
+        assertThat(userModelSearched.get().getId()).isEqualTo(idSearched);
+    }
+
+    @Test
+    public void testListUser(){
+        List<UserModel> userModelList=(List<UserModel>) userRepository.findAll();
+        assertThat(userModelList).size().isGreaterThan(0);
+    }
 }
