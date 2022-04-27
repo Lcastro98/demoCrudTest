df88c6db5b6e9300781437a4777ddd2472959349
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-27 13:57:07 -0500

    Servicio
    Se agrega el servicio de usuario

--- src/main/java/com/sofka/democrudtest/services/UserService.java
+++ src/main/java/com/sofka/democrudtest/services/UserService.java
@@ -1,2 +1,41 @@
-package com.sofka.democrudtest.services;public class UserService {
+package com.sofka.democrudtest.services;
+
+import com.sofka.democrudtest.models.UserModel;
+import com.sofka.democrudtest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+
+
+    public ArrayList<UserModel>  getByPriority(Integer priority) {
+        return userRepository.findByPriority(priority);
+    }
+
+    public boolean deleteUser(Long id) {
+        try{
+            userRepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
 }
