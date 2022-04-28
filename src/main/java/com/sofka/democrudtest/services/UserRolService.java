35f99aef24faf583bb56e2bf02a81c8515293ada
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-27 19:31:34 -0500

    Servicio
    Se agrega el servicio de rol de usuario

--- src/main/java/com/sofka/democrudtest/services/UserRolService.java
+++ src/main/java/com/sofka/democrudtest/services/UserRolService.java
@@ -1,2 +1,43 @@
-package com.sofka.democrudtest.services;public class UserRolService {
+package com.sofka.democrudtest.services;
+
+import com.sofka.democrudtest.models.UserModel;
+import com.sofka.democrudtest.models.UserRolModel;
+import com.sofka.democrudtest.repositories.UserRolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserRolService {
+
+    @Autowired
+    UserRolRepository userRolRepository;
+
+    public ArrayList<UserRolModel> getUserRoles(){
+        return (ArrayList<UserRolModel>) userRolRepository.findAll();
+    }
+
+    public UserRolModel saveUserRol(UserRolModel rol){
+        return userRolRepository.save(rol);
+    }
+
+    public Optional<UserRolModel> getById(Long id){
+        return userRolRepository.findById(id);
+    }
+
+    public UserRolModel updateUserRol(Long id, UserRolModel rol) {
+        rol.setId(id);
+        return userRolRepository.save(rol);
+    }
+
+    public boolean deleteUserRol(Long id) {
+        try{
+            userRolRepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
 }
